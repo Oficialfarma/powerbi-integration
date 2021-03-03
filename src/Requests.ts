571b5db1d60a1c084663448710fb7d4c48329c28
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-03-03 11:19:23 -0300

    Inserido os comentários das funções de Request

--- src/Requests.ts
+++ src/Requests.ts
@@ -9,8 +9,18 @@ interface IRequestDatas
     timeout: number;
 }
 
+/**
+ * @classdesc Create all requests to API vtex
+ * @author Alessandro Lima de Miranda
+ * 
+ */
 export class Requests
 {
+    /**
+     * 
+     * @param param0 an object with all url configurations
+     * @returns a resolve or reject data
+     */
     async makeRequest({ url, queryParams, options, timeout }: IRequestDatas): Promise<Object>
     {
         return Promise.race([
@@ -19,12 +29,18 @@ export class Requests
         ]);
     }
 
+    /**
+     * 
+     * @param url
+     * @param options request headers
+     * @returns An object with orders details
+     */
     async get(url: string, options: IRequestOptions)
     {
         return new Promise((resolve, reject) => {
             fetch(url, options)
                 .then(resp => resolve(resp.json()))
-                    .catch(() => reject(url));
+                    .catch(() => reject(new Error(`Error at-${url}`)));
         })
     }
 
@@ -35,7 +51,11 @@ export class Requests
         });
     }
 
+    /**
+     * 
+     * @param reject reject function
+     * @param url
+     * @return an Error when the api returns spent more than timeout defined
+     */
     errorTimeout = (reject: (err: Error) => any, url: string) => () => reject(new Error(`Timeout at-${url}`));
 }
-
-// module.exports = Requests;
