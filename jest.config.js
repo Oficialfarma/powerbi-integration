c2f3b780dd273f975fa2c10f20a9840ca136a7b0
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-03-03 08:16:36 -0300

    Modificado as configurações dos testes

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  moduleFileExtensions: ['ts', 'js'],
   testMatch: ['**/__tests__/*.spec.+(ts|tsx|js)', '**/*.test.+(ts|tsx|js)'],
   transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest',
+      '^.+\\.(ts|tsx)$': 'ts-jest'
   }
 };
