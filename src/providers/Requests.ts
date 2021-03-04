1a75bf69bf1837a801d1f7daa28ddc436cc2c22f
Alessandro Lima de Miranda <ad.lmiranda2018@gmail.com>
2021-03-04 14:50:50 -0300

    Colocado a classe Requests dentro do provider

---
+++ src/providers/Requests.ts
@@ -0,0 +1,54 @@
+import fetch from "node-fetch";
+import { IRequestOptions } from '../interfaces/IRequestOptions';
+import { IRequestDatas, IRequests } from "../interfaces/IRequests";
+
+/**
+ * @classdesc Create all requests to API vtex
+ */
+export class Requests implements IRequests
+{
+    /**
+     * @param param0 an object with all url configurations
+     * @returns a resolve or reject data
+     */
+    async makeRequest({ url, queryParams, options, timeout }: IRequestDatas): Promise<void>
+    {
+        return Promise.race([
+            this.get(url + queryParams, options),
+            this.timeDelay(url, timeout)
+        ]);
+    }
+
+    /**
+     * 
+     * @param url
+     * @param options request headers
+     * @returns An object with orders detail
+     */
+    async get(url: string, options: IRequestOptions): Promise<void>
+    {
+        return new Promise((resolve, reject) => {
+            fetch(url, options)
+                .then(resp => resolve(resp.json()))
+                    .catch(() => reject(new Error(`Error at-${url}`)));
+        })
+    }
+
+    timeDelay(url: string, timeout: number): Promise<void>
+    {
+        return new Promise((resolve, reject) => {
+            setTimeout(this.errorTimeout(reject, url), timeout);
+        });
+    }
+
+    /**
+     * 
+     * @param reject reject function
+     * @param url
+     * @return an Error when the api returns spent more than timeout defined
+     */
+    errorTimeout(reject: (err: Error) => any, url: string)
+    {
+        return reject(new Error(`Timeout at-${url}`))
+    }
+}
