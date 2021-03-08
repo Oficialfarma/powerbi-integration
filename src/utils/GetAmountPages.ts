582e16ecd66ced4aed669b5f8a0d6ce00a4fa634
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-03-08 12:00:38 -0300

    feita a implementação do método para obter a quantia total de páginas do período

--- src/utils/GetAmountPages.ts
+++ src/utils/GetAmountPages.ts
@@ -1,4 +1,21 @@
+import { IRequestDatas } from "../interfaces/IRequests";
+import { Requests } from "../providers/Requests";
+
 export class GetAmountPages
 {
-    
+    static async getPages(options: IRequestDatas): Promise<number>
+    {
+        let amountPages: number = 1;
+        const requests = new Requests();
+
+        await requests.makeRequest({
+            ...options
+        }).then(resp => {
+            amountPages = resp.paging.pages;
+        }).catch(async () => {
+            await requests.requestErrors();
+        });
+
+        return amountPages;
+    }
 }
