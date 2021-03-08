8d7a3f0fdfe991eaaffa23545f10e38bb6bd6994
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-03-08 10:02:05 -0300

    Removido função de obter data atual e atualizado o parâmetro da função de foratação

--- src/utils/DateFormat.ts
+++ src/utils/DateFormat.ts
@@ -3,15 +3,10 @@ export class DateFormat
     /**
      * 
      * @param date - date string
-     * @returns date object in the ISO string format (YYYY-dd-mmT00:00:00.000Z) formated as the VTEX query params
+     * @returns date object in the ISO string format
      */
     public static dateFormatToQueryParams(date: Date): string
     {
-        return new Date(date).toISOString().replace(/:/g, '%3A');
-    }
-
-    public static getActualTime(): Date 
-    {
-        return new Date();
+        return date.toISOString().replace(/:/g, '%3A');
     }
 }
