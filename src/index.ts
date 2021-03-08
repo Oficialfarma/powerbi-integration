7d3f26c7a7d88427e92408692406a595cf47d625
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-03-08 11:59:03 -0300

    Iniciado a obtenção das queryParams e quantidade de páginas do período

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-async function initOrdersGenerator()
+import { GetAmountPages } from "./utils/GetAmountPages";
+import { GetOptions } from "./utils/GetOptions";
+const options = new GetOptions();
+
+async function initOrdersGeneration()
 {
-    
+    await options.setLastTimeRequestFromJson();
+    let urlAndOptions = options.urlOptions();
+
+    const pages = GetAmountPages.getPages(urlAndOptions);
 }
 
-initOrdersGenerator();
+initOrdersGeneration();
