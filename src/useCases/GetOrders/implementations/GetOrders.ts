2027554f68085cbf1d92006f8f4683eb02eec883
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-03-09 15:28:28 -0300

    alterado os parâmetros da função e feito o lançamento das requisições

--- src/useCases/GetOrders/implementations/GetOrders.ts
+++ src/useCases/GetOrders/implementations/GetOrders.ts
@@ -1,41 +1,66 @@
 import { IGetOrders } from "../../../interfaces/IGetOrders";
-import { IRequestOptions } from "../../../interfaces/IRequestOptions";
+import { IGetOrdersDTO } from "../../../interfaces/IGetOrdersDTO";
+import { Requests } from "../../../providers/Requests";
+
+const requests = new Requests();
 
 export class GetOrders implements IGetOrders
 {
-    async getOrders(
-        url: string,
-        options: IRequestOptions,
-        methodType: "list" | "get",
-        timeout: number,
-        queryParams?: string,
-        orderId?: string[],
-        amountPages?: number
-    ): Promise<void>
+    async getOrders(datas: IGetOrdersDTO): Promise<void>
     {
-        amountPages = amountPages ?? 1;
-
-        let requests = [];
-
-        if(methodType === "list")
+        let request = [];
+       
+        if(datas.methodType === "list")
         {
-            for(let i = 1; i <= amountPages; i++)
+            for(let i = 1; i <= datas.amountPages; i++)
             {
-                requests.push({
-                    url,
-                    queryParams: queryParams + `?page=${i}`,
-                    ...options,
-                    timeout
+                request.push({
+                    url: datas.url,
+                    queryParams: datas.queryParams + `&page=${i}`,
+                    options: datas.options,
+                    timeout: datas.timeout
                 });
-                
             }
         }
         else
         {
-            for(let i = 0; i < orderId.length; i++)
+            for(let i = 0; i < datas.orderId.length; i++)
             {
-                
+                request.push({
+                    url: datas.url + `/${datas.orderId}`,
+                    options: datas.options,
+                    timeout: datas.timeout
+                })
             }
         }
+       
+        const prepareRequests = request.map(params => requests.makeRequest(params));
+        
+        const result = await Promise.allSettled(prepareRequests);
+
+        let allSucceded: Array<any>;
+
+        result.map(elem => {
+            if(elem.status === "fulfilled")
+            {
+                if(datas.methodType === "list")
+                {
+                    // elem.value.list.map((order: { orderId: string }) => {
+                    //     allSucceded.push(order.orderId);
+                    // });
+                }
+                else
+                {
+                    allSucceded.push(elem.value);
+                }
+            }
+            else
+            {
+                requests.requestErrors();
+            }
+        });
+        
+        console.log(allSucceded);
+        // return succeded;
     }
 }
