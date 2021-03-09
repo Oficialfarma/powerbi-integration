883f7f5cfe200660c8156315453b23f66fdd6e47
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-03-09 15:31:13 -0300

    instanciado as classes para obtenção dos pedidos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import createGetOrdersController from "./useCases/GetOrders";
 import { GetAmountPages } from "./utils/GetAmountPages";
 import { GetOptions } from "./utils/GetOptions";
 const options = new GetOptions();
@@ -7,7 +8,13 @@ async function initOrdersGeneration()
     await options.setLastTimeRequestFromJson();
     options.setTimeActualRequest();
     let urlAndOptions = options.urlOptions('listOrders');
-    const pages = GetAmountPages.getPages(urlAndOptions);
+    const pages = await GetAmountPages.getPages(urlAndOptions);
+
+    const getOrders = await createGetOrdersController.handle({
+        ...urlAndOptions,
+        methodType: "list",
+        amountPages: pages
+    });
 }
 
 initOrdersGeneration();
