dfb86567707241666fa135871c4b64a024d09632
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-03-09 15:33:19 -0300

    Inluido parametros a serem passados

--- src/interfaces/IGetOrdersDTO.ts
+++ src/interfaces/IGetOrdersDTO.ts
@@ -3,9 +3,10 @@ import { IRequestOptions } from "./IRequestOptions";
 export interface IGetOrdersDTO
 {
     methodType: "list" | "get";
-    queryParams?: string;
-    orderId?: string;
     url: string;
     options: IRequestOptions;
     timeout: number;
+    queryParams?: string;
+    orderId?: string[];
+    amountPages?: number;
 }
