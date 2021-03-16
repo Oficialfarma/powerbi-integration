70572758cbf859c5ab456a388e8aa7e914236f3d
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-03-16 09:55:49 -0300

    Adicionado as flagas de cobertura de teste

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,26 @@
 module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+  // A list of reporter names that Jest uses when writing coverage reports
+  coverageReporters: [
+    "json",
+    "text",
+    "lcov",
+    "clover"
+  ],
+  // An object that configures minimum threshold enforcement for coverage results
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js'],
