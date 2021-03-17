0c7f0327819dc649c7b9f2c01bcda4d21791edbe
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-03-17 10:38:29 -0300

    Criação das entities baseadas no mer para futura criação de manipulação e inserção no banco

---
+++ src/entities/explicacao.ts
@@ -0,0 +1,14 @@
+var tipo = "Não estão diretamente relacionadas com tabelas do banco de dados";
+
+var campos = `os campos que o cliente tem - propriedade readonly para não ser alterada depois de setada
+todas as propriedades são públicas para acesso em outros pontos`;
+
+var adicionais= "vou checar se o user já existe no caso de uso para não inserir cliente duas vezes";
+
+var repositorio = "no repositorio, no método de verificar a existência, ele vai retornar um <Cliente>"
+var rep = "no método para salvar o cliente ele vai receber um <Cliente>"
+
+var casoUso = "no caso de uso ele vai receber a interface clientRepository";
+
+var implementacao = `no useCase vou instanciar a entitie e depois vou chamar o repositorie pra salvar
+passando a instancia do cliente`;
