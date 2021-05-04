6ca3a225ab04d035c9e36d87e8ce120a1f796162
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-04 16:46:05 -0300

    Iniciado o módulo de acesso ao banco

---
+++ src/repositories/Database.ts
@@ -0,0 +1,79 @@
+import { IDatabaseRepository } from "../interfaces/IDatabaseRepository";
+import * as sql from 'mssql';
+
+export class Database implements IDatabaseRepository
+{
+    private connPool: sql.ConnectionPool;
+    private selectColumns: string[] = [];
+    private tableName: string;
+    private connLimit = 10;
+
+    static for()
+    {
+        return new Database();
+    }
+
+    createConnection()
+    {
+        const config = {
+            user: ',sad',
+            password: process.env.DB_PASS_DEV,
+            server: process.env.DB_SERVER_DEV,
+            database: process.env.DB_NAME_DEV,
+            options: {
+                encrypt: true,
+                enableArithAbort: true
+            }
+        }
+    
+        this.connPool = new sql.ConnectionPool(config);
+
+        return this;
+    }
+
+    select(columns: string)
+    {
+        this.selectColumns.push(columns);
+
+        return this;
+    }
+    from(tableName: string)
+    {
+        this.tableName = tableName;
+
+        return this;
+    }
+
+    async build()
+    {
+        try
+        {
+            const stmt = `SELECT ${this.selectColumns.join(',')} FROM ${this.tableName}`;
+            await this.connPool.connect();
+            const { recordset } = await this.connPool.query(stmt);
+
+            this.connPool.close();
+
+            return recordset;
+        }
+        catch(err)
+        {
+            if(this.connLimit === 0)
+            { 
+                throw new Error("Limite de tentativas excedido");
+            }
+
+            await this.remakeBuild(5000);
+        }   
+    }
+
+    private async remakeBuild(time: number)
+    {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                this.connLimit-=1;
+                resolve(this.build());
+            }, time);
+        });
+    }
+}
