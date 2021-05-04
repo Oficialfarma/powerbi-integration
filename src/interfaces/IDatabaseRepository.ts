ed20d6a3cb0c9e0d18867169ca82fd562a1b25b4
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-04 16:45:43 -0300

    Criado interface do módulo de acesso ao banco de dados

---
+++ src/interfaces/IDatabaseRepository.ts
@@ -0,0 +1,9 @@
+import { Database } from "../repositories/Database";
+
+export interface IDatabaseRepository
+{
+    createConnection(): Database;
+    select(columns: string): Database;
+    from(tableName: string): Database;
+    build(): Promise<object | string[] | string>;
+}
