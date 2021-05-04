7b1baf31a7ab97ddbcba3a7e1b680b278c49f963
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-04 16:44:00 -0300

    Deixado o arquivo como processo filho para geração dos arquivos

---
+++ src/initOrdersGeneration.ts
@@ -0,0 +1,32 @@
+import { IRequestDatas } from './interfaces/IRequests';
+import createGetOrdersController from "./useCases/GetOrders";
+import { GetAmountPages } from "./utils/GetAmountPages";
+import { GetOptions } from "./utils/GetOptions";
+const options = new GetOptions();
+
+async function initOrdersGeneration()
+{
+    let paramsAndOptions: IRequestDatas;
+
+    await options.setLastTimeRequestFromJson();
+    options.setTimeActualRequest();
+    paramsAndOptions = options.urlOptions('listOrders');
+    
+    const pages = await GetAmountPages.getPages(paramsAndOptions);
+    
+    const ordersId = await createGetOrdersController.handle({
+        ...paramsAndOptions,
+        methodType: "list",
+        amountPages: pages
+    });
+
+    paramsAndOptions = options.urlOptions('getOrders');
+
+    const detailedOrders = await createGetOrdersController.handle({
+        ...paramsAndOptions,
+        methodType: "get",
+        orderId: ordersId
+    });
+}
+
+initOrdersGeneration();
