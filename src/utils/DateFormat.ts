278813d6f833ff8659505fb8fe397433ae550cbd
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-05-05 17:26:59 -0300

    Inserido comentários

--- src/utils/DateFormat.ts
+++ src/utils/DateFormat.ts
@@ -1,7 +1,8 @@
 export class DateFormat
 {
     /**
-     * 
+     * @description Converts the date to the iso format that will
+     * be passed in the api's query params
      * @param date - date string
      * @returns date object in the ISO string format
      */
