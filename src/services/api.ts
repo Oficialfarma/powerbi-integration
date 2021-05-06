c37c39a9df2645319cebd2c9ff65bee6283705ac
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-06 11:20:44 -0300

    Adicionado os headers a url base

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: `https://${process.env.VTEXACCOUNTNAME}.vtexcommercestable.com.br/api/oms/pvt/orders/`
+    baseURL: `https://${process.env.VTEXACCOUNTNAME}.vtexcommercestable.com.br/api/oms/pvt/orders/`,
+    method: 'GET',
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-VTEX-API-AppKey": process.env.X_VTEX_API_APPKEY,
+        "X-VTEX-API-AppToken": process.env.X_VTEX_API_APPTOKEN
+    }
 });
