3dc5e643a06285053f950d1cc66e1a8e392465ab
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-06 11:23:16 -0300

    Removido as interfaces dos métodos excluídos

--- src/interfaces/IGetOrdersDTO.ts
+++ src/interfaces/IGetOrdersDTO.ts
@@ -1,10 +1,7 @@
-import { IRequestOptions } from "./IRequestOptions";
-
 export interface IGetOrdersDTO
 {
     methodType: "list" | "get";
     url?: string;
-    options: IRequestOptions;
     timeout: number;
     queryParams?: string;
     orderId?: string[];
--- src/interfaces/IRequests.ts
+++ src/interfaces/IRequests.ts
@@ -1,16 +1,13 @@
-import { IRequestOptions } from "./IRequestOptions";
-
 export interface IRequestDatas
 {
     url?: string;
     queryParams?: string;
-    options: IRequestOptions;
     timeout: number;
 }
 
 export interface IRequests
 {
     makeRequest(datas: IRequestDatas): Promise<void>;
-    get(options: IRequestOptions, url?: string): Promise<object>;
+    get(url?: string): Promise<object>;
     timeDelay(timeout: number): Promise<Error>;
 }
