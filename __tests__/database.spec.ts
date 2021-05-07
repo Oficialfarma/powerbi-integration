3d8d5a0d498e5a1c8dc72db87b8406d1d176f6f0
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-07 11:48:38 -0300

    Criado os testes iniciais do banco de dados

--- __tests__/database.spec.ts
+++ __tests__/database.spec.ts
@@ -1,5 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { describe, expect, test } from "@jest/globals";
 import { Database } from "../src/repositories/Database";
+import * as sql from 'mssql';
 
 jest.unmock('axios');
 
@@ -16,7 +20,42 @@ describe("Database", () => {
         expect(result).toEqual({})
     });
     
-    test.todo("#From - Given a table name, it shoulds return all fields");
+    test("#From - Given a table name, it shoulds return all fields", async () => {
+        const db = Database.for().createConnection();
 
-    test.todo("#Select - Should return only specified columns");
+        const result = await db.from("requestStatus").build();
+
+        const expected = [{
+            id_status: 1,
+            lastTimeRequest: new Date("2021-05-04T11:35:45.480Z"),
+            requestStatus: true
+        }]
+
+        expect(result).toEqual(expected);
+    });
+
+    test("#Select - Should return only specified columns", async () => {
+        const db = Database.for().createConnection();
+
+        const result = await 
+            db.from('requestStatus')
+            .select('lastTimeRequest')
+            .build();
+
+        const expected = [{
+            lastTimeRequest: new Date("2021-05-04T11:35:45.480Z")
+        }];
+
+        expect(result).toStrictEqual(expected);
+    });
+
+    test('#error - should return a request error', async () => {
+        const db = Database.for().createConnection();
+
+        await expect(
+            db.from('fakeTable')
+            .select('anyColumn')
+            .build()
+        ).rejects.toBeInstanceOf(sql.RequestError);
+    })
 })
