0f4c02b7602317e7cf8130b41b742c52b3be67b5
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-07 16:01:10 -0300

    iniciado o listening de saída e mensagem de erro dos processos filhos

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import { fork } from 'child_process';
 import { Database } from './repositories/Database';
 import { DateFormat } from './utils/DateFormat';
+import createFileSystemController from './useCases/FileSystem';
 
 (async function() {
     const child = fork(__dirname + '/initOrdersGeneration.ts', ['normal']);
@@ -13,9 +14,11 @@ import { DateFormat } from './utils/DateFormat';
 
     const { lastTimeRequest } = lastTimeInDb[0];
     
-    const actualTimeRequest = DateFormat.dateFormatToQueryParams(new Date('2021-05-05T13:30:00'));
+    const actualTimeRequest = DateFormat.dateFormatToQueryParams(new Date('2021-05-04T12:00:00'));
     const lastTime = DateFormat.dateFormatToQueryParams(new Date(lastTimeRequest));
-    const queryParams = `?f_creationDate=creationDate%3A%5B${lastTime}%20TO%20${actualTimeRequest}%5D&per_page=100`;
+    
+    // const queryParams = `?f_creationDate=creationDate%3A%5B${lastTime}%20TO%20${actualTimeRequest}%5D&per_page=100`;
+    const queryParams = ``;
 
     child.send({
         queryParams,
@@ -23,10 +26,44 @@ import { DateFormat } from './utils/DateFormat';
         actualTimeRequest
     });
     
-    child.on('exit', status => {
-        if(Boolean(status))
+    let errorMessage: string;
+
+    child.on('message', (message: string) => {
+        if(message.indexOf('Error') !== -1)
         {
-            console.log('erro ', status)
+            errorMessage = message;
+            writeLogError(message + new Date());
+            child.disconnect();
         }
     });
+
+    child.on('exit', async (status) => {
+        if(Boolean(status))
+        {
+            writeLogError("An error has occurred");
+        }
+    });
+
 })();
+
+const retryWriteLogLimit = 5;
+
+async function writeLogError(errorMessage: string)
+{
+    await createFileSystemController.handle({
+        filePath: 'error.log',
+        methodName: 'write',
+        errorMessage
+    }).then(() => {
+        process.exit(0);
+    }).catch((err) => {
+        if(retryWriteLogLimit > 0)
+        {
+            setTimeout(writeLogError, 2000);
+        }
+        else
+        {
+            process.exit();
+        }
+    }); 
+}
