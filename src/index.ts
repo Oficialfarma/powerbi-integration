2b3f483507756e15f56b2fffed7512c343bbf3b4
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-07 14:34:14 -0300

    Iniciado o compartilhamento de informações entre os processos

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,30 @@ dotenv.config();
 
 import { fork } from 'child_process';
 import { Database } from './repositories/Database';
+import { DateFormat } from './utils/DateFormat';
 
-// const child = fork(__dirname + '/initOrdersGeneration.ts', ['normal']);
 (async function() {
+    const child = fork(__dirname + '/initOrdersGeneration.ts', ['normal']);
     const db = Database.for().createConnection();
-    const lastTime = await db.select('lastTimeRequest').from('requestStatus').build();
-    
-    console.log(lastTime)
-})();
 
-// child.on('exit', status => {
-//     if(Boolean(status))
-//     {
-//         console.log('erro ', status)
-//     }
-// });
+    const lastTimeInDb = await db.from('requestStatus').select('lastTimeRequest').build();
+
+    const { lastTimeRequest } = lastTimeInDb[0];
+    
+    const actualTimeRequest = DateFormat.dateFormatToQueryParams(new Date('2021-05-05T13:30:00'));
+    const lastTime = DateFormat.dateFormatToQueryParams(new Date(lastTimeRequest));
+    const queryParams = `?f_creationDate=creationDate%3A%5B${lastTime}%20TO%20${actualTimeRequest}%5D&per_page=100`;
+
+    child.send({
+        queryParams,
+        lastTime,
+        actualTimeRequest
+    });
+    
+    child.on('exit', status => {
+        if(Boolean(status))
+        {
+            console.log('erro ', status)
+        }
+    });
+})();
