f7b0c5de29c35a7721b9660215ba2be97450b1ba
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-07 14:36:26 -0300

    Atualizado o retorno da função build

--- src/interfaces/IDatabaseRepository.ts
+++ src/interfaces/IDatabaseRepository.ts
@@ -5,5 +5,5 @@ export interface IDatabaseRepository
     createConnection(): Database;
     select(columns: string): Database;
     from(tableName: string): Database;
-    build(): Promise<object[] | string[] | object>;
+    build(): Promise<object[] | string[]>;
 }
