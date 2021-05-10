a95172706fe0e2f1d9520d55debfb4e0cf6781b3
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-05-10 11:12:45 -0300

    Trocado de arquivo a função para escrita de log de erros

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import createFileSystemController from './useCases/FileSystem';
     const db = Database.for().createConnection();
 
     const lastTimeInDb = await db.from('requestStatus').select('lastTimeRequest').build();
-
+    
     const { lastTimeRequest } = lastTimeInDb[0];
     
     const actualTimeRequest = DateFormat.dateFormatToQueryParams(new Date('2021-05-04T12:00:00'));
@@ -25,45 +25,16 @@ import createFileSystemController from './useCases/FileSystem';
         lastTime,
         actualTimeRequest
     });
-    
-    let errorMessage: string;
-
-    child.on('message', (message: string) => {
-        if(message.indexOf('Error') !== -1)
-        {
-            errorMessage = message;
-            writeLogError(message + new Date());
-            child.disconnect();
-        }
-    });
 
     child.on('exit', async (status) => {
         if(Boolean(status))
         {
-            writeLogError("An error has occurred");
+            // erro - aqui chama o insert com a última data
+        }
+        else
+        {
+            // se a saída for bem sucedida, atualiza o último horário
         }
     });
 
 })();
-
-const retryWriteLogLimit = 5;
-
-async function writeLogError(errorMessage: string)
-{
-    await createFileSystemController.handle({
-        filePath: 'error.log',
-        methodName: 'write',
-        errorMessage
-    }).then(() => {
-        process.exit(0);
-    }).catch((err) => {
-        if(retryWriteLogLimit > 0)
-        {
-            setTimeout(writeLogError, 2000);
-        }
-        else
-        {
-            process.exit();
-        }
-    }); 
-}
