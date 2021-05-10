36c2dde154b2c5cc17e0038c897712257be1c7c2
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-05-10 17:10:52 -0300

    Atualizado os métodos da classe de acesso ao banco

--- src/interfaces/IDatabaseRepository.ts
+++ src/interfaces/IDatabaseRepository.ts
@@ -5,5 +5,7 @@ export interface IDatabaseRepository
     createConnection(): Database;
     select(columns: string): Database;
     from(tableName: string): Database;
+    insertIntoTable(tableName: string): Database;
+    values(values: object | object[]): Database;
     build(): Promise<object[] | string[]>;
 }
