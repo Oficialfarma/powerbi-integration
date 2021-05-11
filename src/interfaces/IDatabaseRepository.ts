ce880d6d6ff9495a812bddba4a0c068888fb54ca
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-11 17:08:49 -0300

    Atualizado a interface com os novos métodos inseridos

--- src/interfaces/IDatabaseRepository.ts
+++ src/interfaces/IDatabaseRepository.ts
@@ -5,7 +5,10 @@ export interface IDatabaseRepository
     createConnection(): Database;
     select(columns: string): Database;
     from(tableName: string): Database;
-    insertIntoTable(tableName: string): Database;
+    insertInto(tableName: string): Database;
     values(values: object | object[]): Database;
+    update(tablename: string): Database;
+    set(datas: object): Database;
+    where(filter: string): Database;
     build(): Promise<object[] | string[]>;
 }
