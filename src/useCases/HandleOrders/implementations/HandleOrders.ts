df261e9651f1becafaf63c62c17b2d34a067799a
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-18 16:42:53 -0300

    Criado a classe pra manipulação dos pedidos

--- src/useCases/HandleOrders/implementations/HandleOrders.ts
+++ src/useCases/HandleOrders/implementations/HandleOrders.ts
@@ -1,4 +1,54 @@
-export default class HandleOrders
+import { IHandleOrders } from "../../../interfaces/IHandleOrders";
+
+export default class HandleOrders implements IHandleOrders
 {
-    
+    client(order: object)
+    {
+        return {};
+    }
+
+    addressShippingData(order: object)
+    {
+        return {}
+    }
+
+    clientShippingData(order: object)
+    {
+        return {}
+    }
+
+    dicountsName(order: object)
+    {
+        return {}
+    }
+
+    items(order: object)
+    {
+        return {}
+    }
+
+    logisticsInfo(order: object)
+    {
+        return {}
+    }
+
+    orderItems(order: object)
+    {
+        return {}
+    }
+
+    orders(order: object)
+    {
+        return {}
+    }
+
+    paymentData(order: object)
+    {
+        return {}
+    }
+
+    saveOrders(orders: object[])
+    {
+        return false;
+    }
 }
