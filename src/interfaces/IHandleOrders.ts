b5b10dd3a5a89a032439b350460932990f2e08b7
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-18 16:37:09 -0300

    Incluido os métodos que serão implementados na classe

--- src/interfaces/IHandleOrders.ts
+++ src/interfaces/IHandleOrders.ts
@@ -1,4 +1,13 @@
 export interface IHandleOrders
 {
-
+    client(oder: object): object;
+    clientShippingData(order: object): object;
+    addressShippingData(order: object): object;
+    orders(order: object): object;
+    logisticsInfo(order: object): object;
+    paymentData(order: object): object;
+    dicountsName(order: object): object;
+    orderItems(order: object): object;
+    items(order: object): object;
+    saveOrders(orders: object[]): boolean 
 }
