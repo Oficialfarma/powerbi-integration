2f0a11ff53eda802d60dbf4d684c0504ab1f93e5
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-26 15:48:30 -0300

    Finalizado a implementação da manipulação dos pedidos

--- src/useCases/HandleOrders/implementations/HandleOrders.ts
+++ src/useCases/HandleOrders/implementations/HandleOrders.ts
@@ -9,14 +9,14 @@ export default class HandleOrders implements IHandleOrders
     {
         let clientEmail = order.clientProfileData.email;
         clientEmail = clientEmail.substring(0, clientEmail.indexOf('-'));
-
+        
         return {
             Client: {
-                client_id: order.clientProfileData.userProfileId,
-                name: order.clientProfileData.firstName,
-                last_name: order.clientProfileData.lastName,
-                email: clientEmail,
-                document: order.clientProfileData.document
+                client_id: `'${order.clientProfileData.userProfileId}'`,
+                name: `'${order.clientProfileData.firstName}'`,
+                last_name: `'${order.clientProfileData.lastName}'`,
+                email: `'${clientEmail}'`,
+                document: `'${order.clientProfileData.document}'`
             }
         };
     }
@@ -25,11 +25,11 @@ export default class HandleOrders implements IHandleOrders
     {
         return {
             ShippingData: {
-                addressId: order.shippingData.address.addressId,
-                state: order.shippingData.address.state,
-                city: order.shippingData.address.city,
-                receiverName: order.shippingData.address.receiverName,
-                neighborhood: order.shippingData.address.neighborhood
+                addressId: `'${order.shippingData.address.addressId}'`,
+                state: `'${order.shippingData.address.state}'`,
+                city: `'${order.shippingData.address.city}'`,
+                receiverName: `'${order.shippingData.address.receiverName}'`,
+                neighborhood: `'${order.shippingData.address.neighborhood}'`
             }
         }
     }
@@ -38,9 +38,9 @@ export default class HandleOrders implements IHandleOrders
     {
         return {
             Client_ShippingData: {
-                clientShippingId: uuidv4(),
-                client_id: order.clientProfileData.id,
-                address_id: order.shippingData.address.addressId
+                clientShippingId: `'${uuidv4()}'`,
+                client_id: `'${order.clientProfileData.userProfileId}'`,
+                addressId: `'${order.shippingData.address.addressId}'`
             }
         }
     }
@@ -50,9 +50,9 @@ export default class HandleOrders implements IHandleOrders
         const discountsName = order.ratesAndBenefitsData.rateAndBenefitsIdentifiers.map(elem => {
             return {
                 DiscountsName: {
-                    discountId: elem.id,
-                    orderId: order.orderId,
-                    discountName: elem.name
+                    discountId: `'${elem.id}'`,
+                    orderId: `'${order.orderId}'`,
+                    discountName: `'${elem.name}'`
                 }
             };
         });
@@ -65,8 +65,8 @@ export default class HandleOrders implements IHandleOrders
         const itens = order.items.map(item => {
             return {
                 Items: {
-                    skuId: item.id,
-                    skuName: item.name
+                    skuId: `'${item.id}'`,
+                    skuName: `'${item.name}'`
                 }
             };
         });
@@ -78,14 +78,14 @@ export default class HandleOrders implements IHandleOrders
     {
         const logisticsInfo = {
             LogisticsInfo: {
-                logistics_id: uuidv4(),
-                slaType: order.shippingData.logisticsInfo[0].selectedSla,
-                courrier: order.shippingData.logisticsInfo[0].selectedSla,
-                estimateDeliveryDate: order.shippingData.logisticsInfo[0].shippingEstimateDate,
-                deliveryDeadline: order.shippingData.logisticsInfo[0].shippingEstimate,
-                trackingNumber: order.packageAttachment.packages[0].trackingNumber,
-                orderId: order.orderId,
-                addressId: order.shippingData.selectedAddresses[0].addressId
+                logistics_id: `'${uuidv4()}'`,
+                slaType: `'${order.shippingData.logisticsInfo[0].selectedSla}'`,
+                courrier: `'${order.shippingData.logisticsInfo[0].selectedSla}'`,
+                estimateDeliveryDate: `'${order.shippingData.logisticsInfo[0].shippingEstimateDate}'`,
+                deliveryDeadline: `'${order.shippingData.logisticsInfo[0].shippingEstimate}'`,
+                trackingNumber: `'${order.packageAttachment.packages[0].trackingNumber}'`,
+                orderId: `'${order.orderId}'`,
+                addressId: `'${order.shippingData.address.addressId}'`
             }
         };
 
@@ -97,13 +97,13 @@ export default class HandleOrders implements IHandleOrders
         const itens = order.items.map((item, index) => {
             return {
                 Order_Items: {
-                    orderItemsId: `${order.orderId}-${item.id}`,
-                    quantitySold: item.seller,
+                    orderItemsId: `'${order.orderId}-${item.id}'`,
+                    quantitySold: `'${item.seller}'`,
                     skuSellingPrice: item.sellingPrice / 100,
                     skuTotalPrice: (item.sellingPrice * Number(item.seller)) / 100,
                     skuValue: item.costPrice / 100,
-                    orderId: order.orderId,
-                    skuId: item.id,
+                    orderId: `'${order.orderId}'`,
+                    skuId: `'${item.id}'`,
                     shippingValue: order.shippingData.logisticsInfo[index].price / 100,
                     shippingListPrice: order.shippingData.logisticsInfo[index].listPrice / 100
                 }
@@ -120,38 +120,38 @@ export default class HandleOrders implements IHandleOrders
         if(order.callCenterOperatorData !== null)
         {
             callCenterDatas = {
-                callCenterEmail: order.callCenterOperatorData.email,
-                callCenterCode: order.callCenterOperatorData.userName
+                callCenterEmail: `'${order.callCenterOperatorData.email}'`,
+                callCenterCode: `'${order.callCenterOperatorData.userName}'`
             }
         }
         else
         {
             callCenterDatas = {
-                callCenterEmail: null,
-                callCenterCode: null
+                callCenterEmail: "' '",
+                callCenterCode: "' '"
             };
         }
 
         return {
             Orders: {
-                orderId: order.orderId,
-                origin: order.origin,
-                sequence: order.sequence,
-                creation_date: order.creationDate,
-                statusDescription: order.statusDescription,
-                lastChangeDate: order.lastChange,
-                utmSource: order.marketingData.utmSource,
-                utmMedium: order.marketingData.utmMedium,
-                utmCampaign: order.marketingData.utmCampaign,
-                coupon: order.marketingData.coupon,
+                orderId: `'${order.orderId}'`,
+                origin: `'${order.origin}'`,
+                sequence: `'${order.sequence}'`,
+                creation_date: `'${order.creationDate}'`,
+                statusDescription: `'${order.statusDescription}'`,
+                lastChangeDate: `'${order.lastChange}'`,
+                utmSource: `'${order.marketingData.utmSource}'`,
+                utmMedium: `'${order.marketingData.utmMedium}'`,
+                utmCampaign: `'${order.marketingData.utmCampaign}'`,
+                coupon: `'${order.marketingData.coupon}'`,
                 totalValue: order.value / 100,
                 discountsTotals: order.totals.find(totals => {
                     return totals.id === 'Discounts';
                 }).value / 100,
-                host: order.hostname,
-                sellerName: order.sellers[0].name,
+                host: `'${order.hostname}'`,
+                sellerName: `'${order.sellers[0].name}'`,
                 ...callCenterDatas,
-                client_id: order.clientProfileData.userProfileId,
+                client_id: `'${order.clientProfileData.userProfileId}'`,
             }
         }
     }
@@ -161,9 +161,9 @@ export default class HandleOrders implements IHandleOrders
         const payments = order.paymentData.transactions[0].payments.map(payment => {
             return {
                 PaymentData: {
-                    transaction_id: payment.id,
-                    orderId: order.orderId,
-                    paymentSystemName: payment.paymentSystemName,
+                    transaction_id: `'${payment.id}'`,
+                    orderId: `'${order.orderId}'`,
+                    paymentSystemName: `'${payment.paymentSystemName}'`,
                     installments: payment.installments,
                     paymentValue: payment.value / 100
                 }
@@ -173,7 +173,7 @@ export default class HandleOrders implements IHandleOrders
         return payments
     }
 
-    saveOrders(orders: object[])
+    async saveOrders(orders: object[])
     {
         if(!orders.length) return false;
 
@@ -182,15 +182,11 @@ export default class HandleOrders implements IHandleOrders
         orders.forEach((order: any) => {
             const tableName = Object.keys(order)[0];
             
-            const values = Object.entries(order[tableName]);
-            console.log(order[tableName])
             db.insertInto(tableName).values(order[tableName]);
-            // for(const [ key, value ] of values)
-            // {
-                
-            // }
         });
-        
-        return false;
+
+        const response = await db.build().then(resp => resp);
+
+        return response;
     }
 }
