5d427830f5f8d7ce89f375184b3633413b5860d5
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-05-28 15:37:04 -0300

    Criado arquivo para escrita de log de erros

---
+++ src/utils/writeLogError.ts
@@ -0,0 +1,44 @@
+import createFileSystemController from "../useCases/FileSystem";
+
+export default async function writeLogError(errorMessage: string)
+{
+    let retryWriteLogLimit = 5;
+
+    const datas = await createFileSystemController.handle({
+        filePath: 'error.log',
+        methodName: 'read'
+    })
+    .then(resp => resp)
+    .catch(err => err);
+    
+    let message;
+
+    if(datas instanceof Error)
+    {
+        message = errorMessage + " " + new Date();
+    }
+    else
+    {
+        message = datas + '\r\n' + errorMessage + " " + new Date();
+    }
+    
+    await createFileSystemController.handle({
+        filePath: 'error.log',
+        methodName: 'write',
+        errorMessage: message
+    }).then(() => {
+        process.exit(0);
+    }).catch((err) => {
+        if(retryWriteLogLimit > 0)
+        {
+            retryWriteLogLimit-=1;
+            setTimeout(() => {
+                writeLogError(errorMessage)
+            }, 2000);
+        }
+        else
+        {
+            process.exit(0);
+        }
+    }); 
+}
