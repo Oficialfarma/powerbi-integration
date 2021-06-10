ccd80110f02827b358dd04e0b17b5da4aafe9bdf
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-06-10 17:04:25 -0300

    Inserido o Cronjob

--- src/index.ts
+++ src/index.ts
@@ -5,49 +5,72 @@ import { CronJob } from 'cron';
 import { fork } from 'child_process';
 import { Database } from './repositories/Database';
 import { DateFormat } from './utils/DateFormat';
+import createFileSystemController from './useCases/FileSystem';
 
-const job = new CronJob('0 */10 * * * *', () => {
-    console.log('rodou')
+new CronJob('0 */30 * * * *', async () => {
+    console.log('Buscando pedidos')
+    const child = fork(__dirname + '/initOrdersGeneration.ts', ['normal']);
+    const db = new Database().createConnection();
+    
+    const lastTimeInDb = await db.select('lastTimeRequest').from('requestStatus').build();
+    const { lastTimeRequest } = lastTimeInDb[0];
+    
+    // const actualTime = new Date('2021-06-10T16:30:00.000Z');
+    const actualTime = new Date();
+    const actualTimeToRequest = DateFormat.dateFormatToQueryParams(actualTime);
+    const lastTime = DateFormat.dateFormatToQueryParams(new Date(lastTimeRequest));
+    
+    const queryParams = `?f_creationDate=creationDate%3A%5B${lastTime}%20TO%20${actualTimeToRequest}%5D&per_page=100`;
+
+    child.send(queryParams);
+
+    child.on('exit', async (status: number) => {
+        if(Boolean(status))
+        {
+            await db.update('requestStatus').set({
+                lastTimeRequest: `'${actualTime.toISOString()}'`,
+                requestStatus: 1
+            }).where("id_status=1").build();
+        }
+        else
+        {
+            await db.update('requestStatus').set({
+                requestStatus: 0
+            }).where("id_status=1").build();
+        }
+        console.log('Baixou pedidos');
+    });
 }, null, true, 'America/Sao_Paulo');
 
-job.start();
-
-// (async function() {
-//     const child = fork(__dirname + '/initOrdersGeneration.ts', ['normal']);
-//     const db = new Database().createConnection();
-    
-//     const lastTimeInDb = await db.select('lastTimeRequest').from('requestStatus').build();
-//     const { lastTimeRequest } = lastTimeInDb[0];
-    
-//     const actualTimeRequest = DateFormat.dateFormatToQueryParams(new Date('2021-05-27T09:15:00'));
-//     const lastTime = DateFormat.dateFormatToQueryParams(new Date(lastTimeRequest));
-    
-//     const queryParams = `?f_creationDate=creationDate%3A%5B${lastTime}%20TO%20${actualTimeRequest}%5D&per_page=100`;
-//     // const queryParams = ``;
-
-//     child.send(queryParams);
-
-//     child.on('exit', async (status: number) => {
-//         if(Boolean(status))
-//         {
-//             await db.update('requestStatus').set({
-//                 lastTimeRequest: actualTimeRequest,
-//                 requestStatus: 1
-//             }).where("id_status=1").build();
-//         }
-//         else
-//         {
-//             await db.update('requestStatus').set({
-//                 requestStatus: 0
-//             }).where("id_status=1").build();
-//         }
-//     });
-// })();
-
-(async function () {
+new CronJob('0 */25 * * * *', async () => {
     const child = fork(__dirname + '/updateOrders.ts', ['normal']);
+    console.log('Atualizando pedidos')
+    child.on('exit', async (err: number | Error) => {
+        if(Boolean(err))
+        {
+            const data = await createFileSystemController.handle({
+                filePath: 'updateStatus.log',
+                methodName: 'read'
+            })
+            .then(resp => resp)
+            .catch(err => err);
+            
+            let message: string;
 
-    child.on('exit', () => {
-        console.log('saiu');
+            if(data instanceof Error)
+            {
+                message = 'Orders have not been updated ' + new Date();
+            }
+            else
+            {
+                message = data + '\r\nOrders have been updated successfully ' + new Date();
+            }
+            await createFileSystemController.handle({
+                filePath: 'updateStatus.log',
+                methodName: 'write',
+                errorMessage: message
+            });
+        }
+        console.log('Atualizou pedidos');
     })
-})();
+}, null, true, 'America/Sao_Paulo');
