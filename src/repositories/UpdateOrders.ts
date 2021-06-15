5e394653b08d333f8931579b1dc991c31fcf5876
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-06-15 09:49:05 -0300

    Adicionado comentário

--- src/repositories/UpdateOrders.ts
+++ src/repositories/UpdateOrders.ts
@@ -1,7 +1,16 @@
 import { Database } from "./Database";
 
+/**
+ * @classdesc class to create the limit of the orders table
+ */
 export default class UpdateOrders extends Database
 {
+    /**
+     * @description create the limit query
+     * @param actualIndex initial index of clause between
+     * @param maxIndex final index of clause between
+     * @returns class instance
+     */
     specialLimit(actualIndex: number, maxIndex: number)
     {
         const query = `
