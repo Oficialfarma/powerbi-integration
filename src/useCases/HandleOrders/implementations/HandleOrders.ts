9d8e17add7ef7e3269558379db4a9624f878211e
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-06-15 08:52:51 -0300

    Removido o console log do método de salvamento

--- src/useCases/HandleOrders/implementations/HandleOrders.ts
+++ src/useCases/HandleOrders/implementations/HandleOrders.ts
@@ -218,7 +218,7 @@ export default class HandleOrders implements IHandleOrders
         });
 
         const response = await db.build().then(resp => resp);
-        console.log(response);
+        
         return response;
     }
 }
