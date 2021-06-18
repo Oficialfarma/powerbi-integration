0c2ba5628331b5539050a7f7289b16a2aea72175
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-06-18 15:48:54 -0300

    Atualizado o nome do módulo

---
+++ src/utils/removeDoubleItems.ts
@@ -0,0 +1,57 @@
+/**
+ * @description Removes double elements from an Array recursively 
+ * until all items in the array have been checked.
+ * Ps.: For now, it should be a not very large amount of information to avoid stack overflow
+ * @param items Original array with double elements
+ * @param itemsToBeReturned new array with no double elements.
+ * Array passed during recursive call
+ * @returns New array with no double informations
+ */
+export default function removeDoubleItems(items: Array<any>, itemsToBeReturned: Array<any> = []): Array<any>
+{
+    if(items.length > 0)
+    {
+        const actualItem = items.splice(0, 1)[0];
+
+        if(itemsToBeReturned.length !== 0)
+        {
+            let hasDoubleitem = false;
+
+            for(let i = 0; i < itemsToBeReturned.length; i++)
+            {
+                const actualKey: any = Object.keys(itemsToBeReturned[i])[0];
+                const objId = Object.keys(itemsToBeReturned[i][actualKey])[0];
+                const objIdValue = itemsToBeReturned[i][actualKey][objId];
+                const objName = Object.getOwnPropertyNames(actualItem)[0];
+                
+                if(objName === actualKey)
+                {
+                    if(objIdValue === actualItem[actualKey][objId])
+                    {
+                        hasDoubleitem = true;
+                        break;
+                    }
+                }
+                
+                if(JSON.stringify(itemsToBeReturned[i]) === JSON.stringify(actualItem))
+                {
+                    hasDoubleitem = true;
+                    break;
+                }
+            }
+
+            if(!hasDoubleitem)
+            {
+                itemsToBeReturned.push(actualItem);
+            }
+        }
+        else
+        {
+            itemsToBeReturned.push(actualItem);
+        }
+
+        removeDoubleItems(items, itemsToBeReturned);
+    }
+
+    return itemsToBeReturned;
+}
